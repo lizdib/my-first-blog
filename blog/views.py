3be2613769af05ef1ad3adfa39e4963ffeb541b5
Liza <lizdib@yandex.ru>
2018-11-26 23:32:57 +0300

    11

--- blog/views.py
+++ blog/views.py
@@ -28,8 +28,8 @@ def search_form(request):
 def search(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
+        rep = Reply.objects.filter(title__icontains=q)
         return render_to_response('search_results.html',
-            {'books': books, 'query': q})
+            {'rep': rep, 'query': q})
     else:
         return render_to_response('search_form.html', {'error': True})
