ea83d1f3292af54ae5525ecf8b215f3986743f1e
Liza <lizdib@yandex.ru>
2018-12-07 19:08:47 +0300

    31

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     url(r'^Request/(?P<pk>\d+)/edit/$', views.Request_edit, name='Request_edit'),
     url(r'^search_form/$', views.search_form, name='search_form'),
     url(r'^search_results/$', views.search, name='search'),
-    url(r'^Reply/(?P<pk>[0-9])/remove/$', views.Reply_remove, name='Reply_remove'),
-    url(r'^Request/(?P<pk>[0-9])/remove/$', views.Request_remove, name='Request_remove'),
+    url(r'^Reply/(?P<pk>\d+)/remove/$', views.Reply_remove, name='Reply_remove'),
+    url(r'^Request/(?P<pk>\d+)/remove/$', views.Request_remove, name='Request_remove'),
 ]
